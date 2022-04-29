87a27f6700aad0bd89a35650baa57b0c9199699f
medmax33 <m.a.medvedev@outlook.com>
2022-04-29 12:46:14 +0500

    add task_1.py - task 1.1

--- task_1.py
+++ task_1.py
@@ -35,7 +35,23 @@ class LinkedList:
         return [] # здесь будет ваш код
 
     def delete(self, val, all=False):
-        pass # здесь будет ваш код
+        node = self.head
+        node_prev = self.head
+        while node is not None:
+
+            if node.value == val:
+                if node == self.head:
+                    self.head = node.next
+                elif node == self.tail:
+                    node_prev.next = None
+                    self.tail = node_prev
+                else:
+                    node_prev.next = node.next
+                    break
+
+            node_prev = node
+            node = node.next
+        return None
 
     def clean(self):
         pass # здесь будет ваш код
---
+++ task_1_tests.py
@@ -0,0 +1,21 @@
+from task_1 import Node, LinkedList
+
+n1 = Node(12)
+n2 = Node(55)
+n1.next = n2
+s_list = LinkedList()
+s_list.add_in_tail(n1)
+s_list.add_in_tail(n2)
+s_list.add_in_tail(Node(128))
+s_list.add_in_tail(Node(321))
+s_list.add_in_tail(Node(55))
+# s_list.print_all_nodes()
+
+# nf = s_list.find(55)
+# if nf is not None:
+#     print(nf.value)
+
+s_list.print_all_nodes()
+print()
+s_list.delete(55)
+s_list.print_all_nodes()
