b6c97f4b4226382b00153f50e4569d0421b0b1a1
medmax33 <m.a.medvedev@outlook.com>
2022-05-06 11:51:50 +0500

    add task_2.py within 2.3

---
+++ task_2.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+    def __str__(self):
+        return f"[{self.value}]->{self.next}"
+
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+
+    def prnt(self):
+        node = self.head
+        print(node)
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        node = self.head
+        find_all_array = []
+
+        while node is not None:
+            if node.value == val:
+                find_all_array.append(node)
+            node = node.next
+        return find_all_array
+
+    def delete(self, val, all=False):
+        node = self.head
+
+        while node is not None:
+
+            if node.value == val:
+                if node == self.head and node == self.tail:
+                    node.prev = node.next = None
+                    self.head = None
+                    self.tail = None
+                elif node == self.head:
+                    node_next = node.next
+                    self.head = node_next
+                    node.prev = node.next = None
+                    node_next.prev = self.head
+                    node = node_next
+                elif node == self.tail:
+                    node_prev = node.prev
+                    self.tail = node_prev
+                    node_prev.next = None
+                    node.prev = node.next = None
+                    node = node_prev
+                else:
+                    node_prev = node.prev
+                    node_next = node.next
+                    node_prev.next = node_next
+                    node_next.prev = node_prev
+                    node.prev = node.next = None
+                    node = node_next
+                if all is False:
+                    break
+            else:
+                node = node.next
+
+    def clean(self):
+        pass # здесь будет ваш код
+
+    def len(self):
+        return 0 # здесь будет ваш код
+
+    def insert(self, afterNode, newNode):
+        pass # здесь будет ваш код
+
+    def add_in_head(self, newNode):
+        pass # здесь будет ваш код
---
+++ task_2_tests.py
@@ -0,0 +1,190 @@
+import unittest
+import random
+from task_2 import Node, LinkedList2
+
+
+class MyTests(unittest.TestCase):
+
+    # seria of empty tests
+    def test_empty_delete(self):
+        s_list = LinkedList2()
+        s_list.delete(0, False)
+        s_list.prnt()
+        self.assertEqual(s_list.len(), 0)
+
+    def test_empty_clean(self):
+        s_list = LinkedList2()
+        s_list.clean()
+        self.assertEqual(s_list.len(), 0)
+
+    def test_empty_find_all(self):
+        s_list = LinkedList2()
+        self.assertEqual(s_list.find_all(0), [])
+
+    def test_empty_find(self):
+        s_list = LinkedList2()
+        self.assertEqual(s_list.find(0), None)
+
+    def test_empty_len(self):
+        s_list = LinkedList2()
+        self.assertEqual(s_list.len(), 0)
+
+    def test_empty_insert_head(self):
+        s_list = LinkedList2()
+        s_list.insert(None, Node(5))
+        self.assertEqual(s_list.len(), 1)
+
+    def test_empty_insert_body(self):
+        s_list = LinkedList2()
+        n1 = Node(0)
+        s_list.add_in_tail(n1)
+        s_list.insert(n1, Node(5))
+        s_list.prnt()
+        self.assertEqual(s_list.len(), 2)
+
+    # seria of one element tests
+    def test_one_delete(self):
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(13))
+        s_list.prnt()
+        print()
+        s_list.delete(13, False)
+        s_list.prnt()
+        self.assertEqual(s_list.len(), 0)
+
+    def test_one_clean(self):
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(13))
+        s_list.clean()
+        self.assertEqual(s_list.len(), 0)
+
+    def test_one_find_all(self):
+        s_list = LinkedList2()
+        n = Node(13)
+        s_list.add_in_tail(n)
+        self.assertEqual(s_list.find_all(13), [n])
+
+    def test_one_find(self):
+        s_list = LinkedList2()
+        n = Node(13)
+        s_list.add_in_tail(n)
+        self.assertEqual(s_list.find(13), n)
+
+    def test_one_len(self):
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(13))
+        self.assertEqual(s_list.len(), 1)
+
+    def test_one_insert_head(self):
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(13))
+        s_list.insert(None, Node(5))
+        s_list.prnt()
+        self.assertEqual(s_list.len(), 2)
+
+    def test_one_insert_body(self):
+        s_list = LinkedList2()
+        n1 = Node(3)
+        n2 = Node(5)
+        n3 = Node(8)
+        s_list.add_in_tail(n1)
+        s_list.add_in_tail(n2)
+        s_list.add_in_tail(n3)
+        s_list.insert(n2, Node(13))
+        s_list.prnt()
+        self.assertEqual(s_list.len(), 4)
+
+    # seria of 100 element tests
+    def test_100_delete(self):
+        s_list = LinkedList2()
+        for _ in range(100):
+            s_list.add_in_tail(Node(random.randint(1, 20)))
+        s_list.delete(1, False)
+        self.assertTrue(s_list.len() < 100)
+
+    def test_100_clean(self):
+        s_list = LinkedList2()
+        for _ in range(100):
+            s_list.add_in_tail(Node(random.randint(1, 20)))
+        s_list.clean()
+        self.assertEqual(s_list.len(), 0)
+
+    def test_100_find_all(self):
+        s_list = LinkedList2()
+        for _ in range(100):
+            s_list.add_in_tail(Node(random.randint(1, 20)))
+        s_list.prnt()
+        print(s_list.find_all(13))
+        self.assertIsNotNone(s_list.find_all(13))
+
+    def test_100_find(self):
+        s_list = LinkedList2()
+        for _ in range(100):
+            s_list.add_in_tail(Node(random.randint(1, 20)))
+        s_list.prnt()
+        print(s_list.find(13))
+        self.assertIsNotNone(s_list.find(13))
+
+    def test_100_len(self):
+        s_list = LinkedList2()
+        for _ in range(100):
+            s_list.add_in_tail(Node(random.randint(1, 20)))
+        self.assertEqual(s_list.len(), 100)
+
+    def test_100_insert_head(self):
+        s_list = LinkedList2()
+        for _ in range(100):
+            s_list.add_in_tail(Node(random.randint(1, 20)))
+        s_list.insert(None, Node(-1))
+        s_list.prnt()
+        self.assertEqual(s_list.len(), 101)
+
+    def test_100_insert_body(self):
+        s_list = LinkedList2()
+        n1 = Node('ch')
+        # s_list.add_in_tail(n1)
+        for _ in range(20):
+            k = random.randint(1, 5)
+            if k == 5:
+                n2 = n1
+            else:
+                n2 = Node(k)
+            s_list.add_in_tail(n2)
+        s_list.prnt()
+        s_list.insert(n1, Node('-'))
+        s_list.prnt()
+        self.assertTrue(s_list.len() > 19)
+
+    # delete all test
+    def test_delete_all(self):
+        s_list = LinkedList2()
+        for _ in range(4):
+            s_list.add_in_tail(Node(3))
+        for _ in range(3):
+            s_list.add_in_tail(Node(5))
+        s_list.print_all_nodes()
+        print()
+        s_list.delete(5, False)
+        s_list.print_all_nodes()
+        self.assertTrue(s_list.len() < 100)
+
+    def test_insert_tail(self):
+        s_list = LinkedList2()
+        n1 = Node(1)
+        # n2 = Node(2)
+        # n3 = Node(3)
+        # n4 = Node(4)
+        # n5 = Node(5)
+        s_list.add_in_tail(n1)
+        # s_list.add_in_tail(n2)
+        # s_list.add_in_tail(n3)
+        # s_list.add_in_tail(n4)
+        # s_list.add_in_tail(n5)
+        s_list.prnt()
+        s_list.insert(n1, Node('-'))
+        s_list.prnt()
+        self.assertTrue(s_list.len() == 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
