2541bc6a4485d79f1c9a68c6cb899e7da08d36b7
medmax33 <m.a.medvedev@outlook.com>
2022-05-16 20:18:37 +0500

    add bonus task task_210.py

--- task_210.py
+++ task_210.py
@@ -1,14 +1,112 @@
 class Node:
-    def __init__(self, v):
+    def __init__(self, v=None):
         self.value = v
         self.prev = None
         self.next = None
 
-    def __str__(self):
-        return f"[{self.value}]->{self.next}"
+
+class Dummy(Node):
+    def __init__(self, v=None):
+        super().__init__(v)
+
+    def __bool__(self):
+        return False
 
 
 class LinkedList2:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = Dummy('head')
+        self.tail = Dummy('tail')
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def add_in_tail(self, item):
+        before = self.tail.prev
+        item.prev = before
+        item.next = self.tail
+        before.next = item
+        self.tail.prev = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node is not None:
+            if node:
+                print(node.value)
+            node = node.next
+        print()
+
+    def delete(self, val, all=False):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                node.prev.next = node.next
+                node.next.prev = node.prev
+                if all is False:
+                    return None
+            node = node.next
+        return None
+
+    def insert(self, afterNode, newNode):
+        if afterNode is None and self.head.next == self.tail:
+            self.head.next = newNode
+            self.tail.prev = newNode
+            newNode.prev = self.head
+            newNode.next = self.tail
+            return None
+
+        if afterNode is None:
+            before = self.tail.prev
+            before.next = newNode
+            self.tail.prev = newNode
+            newNode.prev = before
+            newNode.next = self.tail
+            return None
+
+        node = self.head
+        while node is not None:
+            if node == afterNode:
+                break
+            if node.next is None:
+                return None
+            node = node.next
+
+        after = node.next
+        node.next = newNode
+        after.prev = newNode
+        newNode.prev = node
+        newNode.next = after
+        return None
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        node = self.head
+        find_all_array = []
+        while node is not None:
+            if node.value == val:
+                find_all_array.append(node)
+            node = node.next
+        return find_all_array
+
+    def len(self):
+        index = 0
+        node = self.head
+        while node is not None:
+            if node:
+                index += 1
+            node = node.next
+        return index
+
+    def add_in_head(self, newNode):
+        first = self.head.next
+        self.head.next = newNode
+        first.prev = newNode
+        newNode.prev = self.head
+        newNode.next = first
+        return None
---
+++ task_210_tests.py
@@ -0,0 +1,80 @@
+import unittest
+from task_210 import *
+
+
+class MyTests(unittest.TestCase):
+
+    def test_me(self):
+        s_list = LinkedList2()
+        s_list.print_all_nodes()
+        s_list.add_in_tail(Node(5))
+        s_list.print_all_nodes()
+        s_list.add_in_tail(Node(10))
+        s_list.print_all_nodes()
+        # self.assertEqual(s_list.len(), 0)
+
+    def test_delete(self):
+        s_list = LinkedList2()
+        s_list.add_in_tail(Node(5))
+        s_list.add_in_tail(Node(10))
+        s_list.add_in_tail(Node(15))
+        s_list.add_in_tail(Node(5))
+        s_list.print_all_nodes()
+        s_list.delete(5, True)
+        s_list.print_all_nodes()
+
+        # self.assertEqual(s_list.len(), 0)
+
+    def test_insert(self):
+        s_list = LinkedList2()
+        n1 = Node(11)
+        n2 = Node(22)
+        n3 = Node(33)
+        n4 = Node(44)
+        n5 = Node(55)
+        s_list.add_in_tail(n1)
+        s_list.add_in_tail(n2)
+        s_list.add_in_tail(n3)
+        s_list.add_in_tail(n4)
+        s_list.add_in_tail(n5)
+        # s_list.insert(None, Node('!'))
+        s_list.print_all_nodes()
+        s_list.insert(n5, Node('-'))
+        s_list.print_all_nodes()
+
+    def test_find(self):
+        s_list = LinkedList2()
+        print(s_list.len())
+        n1 = Node(11)
+        n2 = Node(22)
+        n3 = Node(33)
+        n4 = Node(44)
+        n5 = Node(55)
+        n6 = Node(33)
+
+        s_list.add_in_tail(n1)
+        s_list.add_in_tail(n2)
+        s_list.add_in_tail(n3)
+        s_list.add_in_tail(n4)
+        s_list.add_in_tail(n5)
+        s_list.add_in_tail(n6)
+        print(s_list.find_all(33))
+        print(s_list.len())
+
+    def test_add_in_head(self):
+        s_list = LinkedList2()
+        s_list.add_in_head(Node('1-1-1'))
+        s_list.print_all_nodes()
+        n1 = Node(11)
+        n2 = Node(22)
+        n3 = Node(33)
+        s_list.add_in_tail(n1)
+        s_list.add_in_tail(n2)
+        s_list.add_in_tail(n3)
+        s_list.print_all_nodes()
+        s_list.add_in_head(Node(74747474))
+        s_list.print_all_nodes()
+
+if __name__ == '__main__':
+    unittest.main()
+
