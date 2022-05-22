9a20b329b2034a2638a3f2490f194dfaf804a349
medmax33 <m.a.medvedev@outlook.com>
2022-05-22 08:56:49 +0500

    add task_4.py with balanced function

--- task_4_tests.py
+++ task_4_tests.py
@@ -1,40 +1,9 @@
 import unittest
 # from task_4_head_last import Stack
-from task_4_head_first import Stack
+from task_4 import Stack, balanced
 
 
 class MyTests(unittest.TestCase):
-    # tests for task_4_head_last
-    # def test_head_last_empty_size(self):
-    #     stack = Stack()
-    #     self.assertEqual(stack.size(), 0)
-    #
-    # def test_head_last_empty_pop(self):
-    #     stack = Stack()
-    #     self.assertEqual(stack.pop(), None)
-    #
-    # def test_head_last_empty_peek(self):
-    #     stack = Stack()
-    #     self.assertEqual(stack.peek(), None)
-    #
-    # def test_head_last_empty_push(self):
-    #     stack = Stack()
-    #     stack.push(2)
-    #     self.assertEqual(stack.size(), 1)
-    #     self.assertEqual(stack.peek(), 2)
-    #     self.assertEqual(stack.pop(), 2)
-    #
-    # def test_head_last_5_unit_stack(self):
-    #     stack = Stack()
-    #     stack.push(1)
-    #     self.assertEqual(stack.size(), 1)
-    #     for i in range(2, 6):
-    #         stack.push(i)
-    #     self.assertEqual(stack.size(), 5)
-    #     self.assertEqual(stack.pop(), 1)
-    #     self.assertEqual(stack.peek(), 5)
-    #     stack.pop()
-    #     self.assertEqual(stack.size(), 3)
 
     # tests for task_4_head_first
     def test_head_first_empty_size(self):
@@ -67,6 +36,9 @@ class MyTests(unittest.TestCase):
         self.assertEqual(stack.peek(), 5)
         stack.pop()
         self.assertEqual(stack.size(), 3)
+
+    def test_Balanced(self):
+        self.assertEqual(balanced('(()((()())()))'), True)
 
 
 if __name__ == '__main__':
