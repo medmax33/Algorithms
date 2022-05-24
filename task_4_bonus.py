bdda021651b80bdfd73cc0cdb00bfcd942b88dc5
medmax33 <m.a.medvedev@outlook.com>
2022-05-24 22:17:53 +0500

    add new task_4_bonus.py

--- task_4_bonus.py
+++ task_4_bonus.py
@@ -4,15 +4,18 @@ from task_4 import Stack
 def postfix(s: str) -> int:
     stack1 = Stack()
     stack2 = Stack()
-    for i in s:
-        if i == ' ':
-            continue
-        elif i.isdigit():
-            stack1.push(int(i))
-        elif i in {'+', '*', '='}:
-            stack1.push(i)
-        else:
-            return 1000000000000
+
+    # refactored queue of appending elements to stack
+    # because of my mistake in main task
+    for i in range(len(s) - 1, -1, -1):
+        if s[i].isdigit():
+            stack1.push(int(s[i]))
+        elif s[i] in {'+', '*', '-', '/', '='}:
+            stack1.push(s[i])
+        # if element not digit, arithmetic operation or space
+        # we have an error
+        elif s[i] != ' ':
+            raise TypeError
 
     while stack1.size() > 0:
         element = stack1.pop()
@@ -22,5 +25,18 @@ def postfix(s: str) -> int:
             stack2.push(stack2.pop() + stack2.pop())
         elif element == '*':
             stack2.push(stack2.pop() * stack2.pop())
-        else:
+        elif element == '-':
+            # we pop stack elements in reverse order
+            # first Вычитаемое second Уменьшаемое
+            stack2.push(-stack2.pop() + stack2.pop())
+        elif element == '/':
+            # we pop stack elements in reverse order
+            # first Делитель second Делимое
+            stack2.push((1 / stack2.pop()) * stack2.pop())
+        elif element == '=':
             return stack2.pop()
+
+    if stack2.size() == 1:
+        return stack2.pop()
+
+    raise RuntimeError('Отсуствует завершающий оператор')
